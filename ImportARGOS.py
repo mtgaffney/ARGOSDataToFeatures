bff18596a5af3163d44228ce7e8bc765cbfdc5db
mtgaffney <89404474+mtgaffney@users.noreply.github.com>
2021-10-23 11:21:29 -0400

    Creates an empty feature class

--- ImportARGOS.py
+++ ImportARGOS.py
@@ -7,43 +7,61 @@
 ## Usage: ImportArgos <ARGOS folder> <Output feature class> 
 ##
 ## Created: Fall 2021
-## Author: michael.gaffney@duke.edu (for ENV859)
+## Author: John.Fay@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
 
 # Import modules
 import sys, os, arcpy
+#Allow outputs to overwritten
+arcpy.env.overwriteOutput = True
 
 # Set input variables (Hard-wired)
-inputFile = 'C:/Users/mtgaf/Documents/NicholasSchool/GISDataAnalytics/ARGOSTracking/Data/ARGOSData/1997dg.txt'
-outputFC = "C:/Users/mtgaf/Documents/NicholasSchool/GISDataAnalytics/ARGOSTracking/Scratch/ARGOStrack.shp"
+inputFile = r'C:\Users\mtgaf\Documents\NicholasSchool\GISDataAnalytics\ARGOSTracking\data\ARGOSdata\1997dg.txt'
+outputFC = r'C:\Users\mtgaf\Documents\NicholasSchool\GISDataAnalytics\ARGOSTracking\scratch\ARGOStrack.shp'
 
-#%% Construct a while loop to iterare through all lines in the datafile
-#Open the ARGOS data file for reading
-inputFileObj = open(inputFile, 'r')
+#Create feature class to which we will add the features
+#First split file path into two varaivles to fit the tool properly
+outPath, outFile = os.path.split(outputFC)
+#Create feature class
+arcpy.management.CreateFeatureclass(outPath, outFile)
 
-# Get the first line of data, so we can use a while loop
+#%% Construct a while loop and iterate through all lines in the data file
+# Open the ARGOS data file
+inputFileObj = open(inputFile,'r')
+
+# Get the first line of data, so we can use the while loop
 lineString = inputFileObj.readline()
 
 #Start the while loop
 while lineString:
+    
     # Set code to run only if the line contains the string "Date: "
     if ("Date :" in lineString):
+        
         # Parse the line into a list
         lineData = lineString.split()
+        
         # Extract attributes from the datum header line
         tagID = lineData[0]
-        obsDate = lineData[3]
-        obsTime = lineData[4]
-        obsLC = lineData[7]
+        
         # Extract location info from the next line
         line2String = inputFileObj.readline()
+        
         # Parse the line into a list
         line2Data = line2String.split()
+        
         # Extract the date we need to variables
         obsLat = line2Data[2]
         obsLon= line2Data[5]
+                    
+        # Extract the date, time, and LC values
+        obsDate = lineData[3]
+        obsTime = lineData[4]
+        obsLC   = lineData[7]
+        
         # Print results to see how we're doing
-        print (tagID, obsDate, obsTime, obsLC, "Lat:"+obsLat,"Long:"+obsLon)
+        print (tagID,"Lat:"+obsLat,"Long:"+obsLon, obsLC, obsDate, obsTime)
+        
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
     
